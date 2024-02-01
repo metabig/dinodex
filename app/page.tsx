5287ffdd4d1980f2bc144d6987ded01239c4e133
Antoni Bergas Galmés <c-antoni.bergas@tifin.com>
2024-02-02 00:11:51 +0100

    Add GroupCard component and update Index page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 
+function GroupCard({ grup, position }) {
+  return <div className="min-h-28 flex bg-white p-3 rounded-md shadow-sm gap-x-4">
+    <span className="self-center text-gray-300 text-lg font-semibold">{position}</span>
+    <div className="self-center flex justify-between w-full gap-x-3">
+      <span className="break-normal self-center text-gray-600 text-normal font-medium">{grup.nom}</span>
+      <div className='flex flex-col text-center self-center'>
+        <span className='text-2xl font-bold text-gray-800'>3</span>
+        <span className="text-gray-400 text-xs font-medium w-[30%] text-right">Dinosaures</span>
+      </div>
+    </div>
+  </div>
+}
+
 export default async function Index() {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore);
   const { data: grups } = await supabase.from("Grups").select();
 
-  return <pre>{JSON.stringify(grups, null, 2)}</pre>
+  return <>
+    <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-6xl p-6">
+      Correplaces 2024
+    </h1>
+    <div className="flex flex-col gap-y-2 p-5 w-full">
+      {grups && grups.map((grup, index) => <GroupCard key={index} grup={grup} position={index + 1} />)}
+    </div>
+  </>
 }
--- app/register/[slug]/page.tsx
+++ app/register/[slug]/page.tsx
@@ -1,10 +1,24 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
+import Image from 'next/image'
+import dino from '../../public/dino.jpg'
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const cookieStore = cookies()
   const supabase = createClient(cookieStore);
   const { data: dinos } = await supabase.from("Dino").select().eq("codi_random", params.slug).single();
 
-  return <pre>{dinos.nom_comu}, codi: {dinos.codi_random}</pre>
+  // return <pre>{dinos.nom_comu}, codi: {dinos.codi_random}</pre>
+  return <div className='flex flex-col p-4'>
+    <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-6xl py-2">
+      {dinos.nom_comu}
+    </h1>
+    <Image className="shadow-md rounded-md" src={dino.src} width={500} height={500} alt={''} />
+    <h2 className="mt-2 text-sm font-bold tracking-tight text-gray-900 sm:text-2xl md:text-4xl py-2">
+      Descripció
+    </h2>
+    <p className='text-sm text-gray-500'>
+      {dinos.descripcio ? dinos.descripcio : 'Aquest dinosaure sobra, i la seva descripció també.'}
+    </p>
+  </div>
 }
