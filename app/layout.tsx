ef70056ed3eb9e12e5083dbcb08be1d0255204bc
Antoni Bergas Galmés <c-antoni.bergas@tifin.com>
2024-02-29 00:16:21 +0100

    White bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
+      <body className="bg-white text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
