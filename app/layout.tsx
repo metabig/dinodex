07d709c059c563b28e174feeb725a184f9a90e1e
Antoni Bergas Galmés <c-antoni.bergas@tifin.com>
2024-02-29 01:09:19 +0100

    style: modal text

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Correplaces 2024",
+  description: "Visca el correplaces!",
 };
 
 export default function RootLayout({
--- app/register/[slug]/modal.tsx
+++ app/register/[slug]/modal.tsx
@@ -27,25 +27,25 @@ export default function Modal({ grups, dino }: any) {
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t">
                   <h3 className="text-3xl font-semibold">
                     Dinosaure trobat!
                   </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
                   </button>
                 </div>
                 <form action={addDinoToGroupClient}>
                   <div className="relative p-6 flex-auto">
-                    <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    <p className="my-4 text-black text-lg leading-relaxed">
                       A continuació, selecciona el grup al que vols afegir aquest dinosaure.
                     </p>
-                    <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <div className="w-full md:w-1/3 mb-6 md:mb-0">
                       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
                         Grup
                       </label>
